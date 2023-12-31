82d35053a6cf641253aa3c0702b8083a6c2523fc
suavementee <sofft16hj@gmail.com>
2023-12-31 10:05:48 -0600

    proyecto inventario v2

--- src/main/java/com/example/proyectoApps/ProyectoAppsApplication.java
+++ src/main/java/com/example/proyectoApps/ProyectoAppsApplication.java
@@ -19,7 +19,7 @@ public class ProyectoAppsApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8100")
+                        .allowedOrigins("http://sql3.freesqldatabase.com:3306")
                         .allowCredentials(true)
                         .allowedHeaders("Origin", "Authorization", "Content-Type")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
