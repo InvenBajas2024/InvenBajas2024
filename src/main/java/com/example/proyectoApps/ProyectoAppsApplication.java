553b02decb8c1222d52f887309f0f8e61a1ac826
InvenBajas2024 <inventariobajas@gmail.com>
2024-05-16 10:35:40 -0600

    Update ProyectoAppsApplication.java

--- src/main/java/com/example/proyectoApps/ProyectoAppsApplication.java
+++ src/main/java/com/example/proyectoApps/ProyectoAppsApplication.java
@@ -19,7 +19,7 @@ public class ProyectoAppsApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8100", "http://localhost", "https://inventario-bajas-2024.azurewebsites.net")
+                        .allowedOrigins(*)
                         .allowCredentials(true)
                         .allowedHeaders("Origin", "Authorization", "Content-Type")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
